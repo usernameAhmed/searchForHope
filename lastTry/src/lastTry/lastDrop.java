23e44976309f70cc2e8381095bca934611e6b263
Ahmed Hussein <ah011235813@gmail.com>
2022-03-25 00:46:42 +0200

    there is

--- lastTry/src/lastTry/lastDrop.java
+++ lastTry/src/lastTry/lastDrop.java
@@ -3,5 +3,6 @@ package lastTry;
 public class lastDrop {
 	public static void main(String[] args) {
 		System.out.println("there is hope");
+		//a lot of changes
 	}
 }
---
+++ lastTry/src/yeaah/yes.java
@@ -0,0 +1,7 @@
+package yeaah;
+
+public class yes {
+	public static void main(String[] args) {
+		System.out.println("yes yes");
+	}
+}
