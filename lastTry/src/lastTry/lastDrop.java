bb6d458661cef96538c114eb41ba948b96191424
3amrr <3amrr@DESKTOP-T6VILR8>
2022-03-25 14:22:11 +0200

    etfadal ya usernameahmed

--- lastTry/src/lastTry/lastDrop.java
+++ lastTry/src/lastTry/lastDrop.java
@@ -3,6 +3,7 @@ package lastTry;
 public class lastDrop {
 	public static void main(String[] args) {
 		System.out.println("there is hope");
+		System.out.print("accepted");
 		//a lot of changes
 	}
 }
